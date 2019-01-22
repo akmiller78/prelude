e68f2cc2f81fa2610c0bcbdb0988f32a9899dc37
Adam Miller <akmiller@gmail.com>
2019-01-22 09:56:02 -0600

    personal updates to prelude

--- personal/akmiller78.el
+++ personal/akmiller78.el
@@ -6,3 +6,16 @@
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
 
+;; add aggressive indent mode
+;; dracula theme shouldn't be here as we set the theme in preloads but install it here
+;; should figure out how to do the install in the pre-loads
+(prelude-require-packages '(aggressive-indent dracula-theme))
+
+;; enable agressive indent for clojure
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+
+;; enable smartparens for clojure
+(add-hook 'clojure-mode-hook #'smartparens-strict-mode)
+
+;; enable rainbow parens for clojure
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
